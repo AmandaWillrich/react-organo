de4bd3747b3de867ab5636079056d2f2e0736547
Amanda Willrich Fuchs <111703865+AmandaWillrich@users.noreply.github.com>
2023-02-14 22:12:01 -0300

    textfield

--- organo/src/components/Form/index.js
+++ organo/src/components/Form/index.js
@@ -1,14 +1,34 @@
+import Button from '../Button';
+import DropdownList from '../DropdownList';
 import TextField from '../TextField';
 import './Form.css'
 
 const Form = () => {
+
+    const teams = [
+        'Programação',
+        'Front-End',
+        'Data-Science',
+        'DevOps',
+        'UX e Design',
+        'Mobile',
+        'Inovação e Gestão'
+    ]
+
+    const onSave = (event) => {
+        event.preventDefault()
+        console.log('Form was submitted')
+    }
+
     return (
         <section className="form">
-            <form>
+            <form onSubmit={onSave}>
                 <h2>Preencha os dados para criar o card do colaborador</h2>
-                <TextField label="Nome" placeholder="Digite seu nome" />
-                <TextField label="Cargo" placeholder="Digite seu cargo" />
-                <TextField label="Imagem" placeholder="Informe o endereço da imagem" />
+                <TextField required={true} label="Nome" placeholder="Digite seu nome" />
+                <TextField required={true} label="Cargo" placeholder="Digite seu cargo" />
+                <TextField required={true} label="Imagem" placeholder="Informe o endereço da imagem" />
+                <DropdownList required={true} label="Time" items={teams}/>
+                <Button text="Criar Card" />
             </form>
         </section>
     )
